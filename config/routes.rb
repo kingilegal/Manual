e0f40bd9e312b3588f061e1fdc34e22a624e2abc
Lauro <kinglauro@hotmail.com>
2018-11-03 11:32:28 -0500

    se agrega index y scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  get 'home/index'
+  resources :guardians
+  resources :reports
+  resources :students
+  resources :teachers
+  get 'home/index'
+  root 'home#index'
+  resources :grades
+  resources :institutes
+  resources :districts
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
